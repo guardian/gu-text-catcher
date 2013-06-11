edaedc8dea1528cb7f5aef8f63428d6b50099fd4
Robert Rees <Robert Rees>
2013-06-11 22:57:32 +0100

    Creates a working capture

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import jinja2
 import os
 import json
 import logging
+import hashlib
+
 from urllib import quote, urlencode
 from google.appengine.api import urlfetch
+
+from models import CapturedSelection
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
@@ -17,5 +21,14 @@ class MainPage(webapp2.RequestHandler):
 
 		self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+class CaptureHandler(webapp2.RequestHandler):
+	def post(self):
+		selection = self.request.get("selection")
+		if selection:
+			sha = hashlib.sha1(selection).hexdigest()
+			selection_record = CapturedSelection(text=selection, checksum=sha)
+			selection_record.put()
+
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/capture', CaptureHandler)],
                               debug=True)
--- models.py
+++ models.py
@@ -3,3 +3,8 @@ from google.appengine.ext import ndb
 class Configuration(ndb.Model):
 	key = ndb.StringProperty()
 	value = ndb.StringProperty()
+
+class CapturedSelection(ndb.Model):
+	checksum = ndb.StringProperty()
+	text = ndb.StringProperty()
+	datetime = ndb.DateTimeProperty(auto_now_add=True)
