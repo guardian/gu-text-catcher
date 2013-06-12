3e964279e2473bee077d1a1cff1b68ccf730c93d
Robert Rees <robert.rees@guardian.co.uk>
2013-06-12 08:46:11 +0100

    Adds more data recording so I can build summaries easily

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import os
 import json
 import logging
 import hashlib
+import datetime
 
 from urllib import quote, urlencode
 from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
 
-from models import CapturedSelection
+import headers
+from models import CapturedSelection, Text, Summary
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
@@ -24,10 +27,35 @@ class MainPage(webapp2.RequestHandler):
 class CaptureHandler(webapp2.RequestHandler):
 	def post(self):
 		selection = self.request.get("selection")
-		if selection:
+		today = datetime.date.today().isoformat()
+		logging.info(today)
+		if selection and len(selection) <= 500:
 			sha = hashlib.sha1(selection).hexdigest()
 			selection_record = CapturedSelection(text=selection, checksum=sha)
 			selection_record.put()
+
+			text_key = ndb.Key("Text", sha)
+			text_entity = text_key.get()
+
+			if not text_entity:
+				Text(id=sha, text=selection).put()
+
+			count_key_id = today + "." + sha
+
+			count_key = ndb.Key('Summary', count_key_id)
+
+			count = count_key.get()
+
+			if count:
+				count.count = count.count + 1
+				count.put()
+			else:
+				Summary(id=count_key_id, text=selection, checksum=sha, count=1, size=len(selection)).put()
+
+
+
+
+		headers.set_cors_headers(self.response)
 
 
 app = webapp2.WSGIApplication([('/', MainPage), ('/capture', CaptureHandler)],
--- models.py
+++ models.py
@@ -5,6 +5,16 @@ class Configuration(ndb.Model):
 	value = ndb.StringProperty()
 
 class CapturedSelection(ndb.Model):
-	checksum = ndb.StringProperty()
-	text = ndb.StringProperty()
+	checksum = ndb.StringProperty(required=True)
+	text = ndb.StringProperty(required=True)
 	datetime = ndb.DateTimeProperty(auto_now_add=True)
+
+class Text(ndb.Model):
+	text = ndb.StringProperty(required=True)
+
+class Summary(ndb.Model):
+	checksum = ndb.StringProperty(required=True)
+	text = ndb.StringProperty(required=True)
+	count = ndb.IntegerProperty(default=0)
+	size = ndb.IntegerProperty(required=True)
+	date = ndb.DateProperty(auto_now_add=True)
