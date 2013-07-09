71bd1f86669c5f999e2393b6a4e01ae91243d04a
Robert Rees <robert.rees@guardian.co.uk>
2013-07-09 17:58:50 +0100

    Starting to expose the historical archive via the API and the url structure

--- api.py
+++ api.py
@@ -20,6 +20,17 @@ from models import CapturedSelection, Text, Summary
 from tuples import SummaryInfo
 from formats import jsonp
 
+def read_summary_data(target_date):
+	all_query = Summary.query(Summary.date == target_date, Summary.count > 5).order(-Summary.count)
+
+	def summary_as_map(summary):
+		return {"count" : x.count,
+			"text" : x.text,
+			"detail_url" : "http://gu-text-catcher.appspot.com/content/{id}".format(id=x.checksum),
+			}
+
+	return [summary_as_map(x) for x in all_query.iter()]
+
 class AllTodayHandler(webapp2.RequestHandler):
 	def get(self):
 		headers.json(self.response)
@@ -32,17 +43,7 @@ class AllTodayHandler(webapp2.RequestHandler):
 
 			today = datetime.date.today()
 
-			all_query = Summary.query(Summary.date == today).order(-Summary.count)
-
-			def summary_as_map(summary):
-				return {"count" : x.count,
-					"text" : x.text,
-					"detail_url" : "http://gu-text-catcher.appspot.com/content/{id}".format(id=x.checksum),
-					}
-
-			all_data = [summary_as_map(x) for x in all_query.iter()]
-
-			json_result = json.dumps(all_data)
+			json_result = json.dumps(read_summary_data(today))
 
 			try:
 				memcache.set(cache_key, json_result, 60)
@@ -53,5 +54,32 @@ class AllTodayHandler(webapp2.RequestHandler):
 
 		self.response.out.write(jsonp(self.request, json_result))
 
-app = webapp2.WSGIApplication([('/api/all', AllTodayHandler),],
+class HistoricDayHandler(webapp2.RequestHandler):
+	def get(self, target_date):
+		headers.json(self.response)
+
+		cache_key = "json.%s" % target_date
+
+		json_result = memcache.get(cache_key)
+
+		if not json_result:
+
+			archive_date = datetime.datetime.strptime(target_date, '%Y-%m-%d')
+
+			json_result = json.dumps(read_summary_data(archive_date))
+
+			try:
+				memcache.set(cache_key, json_result, 60)
+			except:
+				pass
+
+		headers.set_cache_headers(self.response, 60)
+
+		self.response.out.write(jsonp(self.request, json_result))
+
+
+app = webapp2.WSGIApplication([
+	('/api/all', AllTodayHandler),
+	webapp2.Route('/api/archive/<target_date:\d{4}-\d{2}-\d{2}>',
+		handler=HistoricDayHandler),],
                               debug=True)
